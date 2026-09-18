44f4ae3f561eb1553c64ad4a1ec63662d4658d98
Mad1yarmaker <madiyarbektaiyli2018@gmail.com>
2026-09-18 14:49:21 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+a = 10;
+b = 'заправка 400 метров от вас!';
+let mst = 'iasigma';
+f = 'test';
+let num = parsefloat('12.5px');
+
+"use strict";
+alert(b);
+"use strict";
+alert(a ** 4);
+"use strict";
+alert(mst.length);
+console.log(f);
+alert(num);
+
